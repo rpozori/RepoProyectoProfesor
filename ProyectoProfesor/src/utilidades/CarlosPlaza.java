a6ee9dcf48761f2a13fc1438495e8a3e7202a685
Carlos <carlos@sincorreo.com>
2015-03-11 14:12:55 +0100

    Modificacion 3 Carlos Plaza

--- ProyectoProfesor/src/utilidades/CarlosPlaza.java
+++ ProyectoProfesor/src/utilidades/CarlosPlaza.java
@@ -5,7 +5,7 @@ public class CarlosPlaza {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Esta es la aportacion de Carlos Plaza");
-		System.out.println("Esta es la aportacion de Carlos Plaza" 2);
+		System.out.println("Esta es la aportacion de Carlos Plaza 2");
 	}
 
 }
