4d4f2f18d564c9b5e1f306e2a5b17c28469c3d48
René <rene@sincorreo.com>
2015-03-11 09:51:19 +0100

    Segunda modificacion

--- ProyectoProfesor/src/utilidades/Principal.java
+++ ProyectoProfesor/src/utilidades/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Este es el proyecto del profesor");
+		System.out.println("Segunda");
 	}
 
 }
