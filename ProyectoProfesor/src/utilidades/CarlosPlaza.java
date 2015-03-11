3ce9d30e81dfbb3ce72c01f14d2f89c0db467e6a
Carlos <carlos@sincorreo.com>
2015-03-11 13:55:01 +0100

    Nueva clase de Carlos Plaza

---
+++ ProyectoProfesor/src/utilidades/CarlosPlaza.java
@@ -0,0 +1,10 @@
+package utilidades;
+
+public class CarlosPlaza {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Esta es la aportacion de Carlos Plaza");
+	}
+
+}
